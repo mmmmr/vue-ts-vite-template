469123f159f23ee32b28e982f6d98e45fdf7d4ce
mmmmr <ob12564@163.com>
2023-05-11 22:00:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+  <el-form :model="form" label-width="120px">
+    <el-form-item label="name">
+      <el-input v-model="form.name" />
+    </el-form-item>
+    <el-form-item label="password">
+      <el-input v-model="form.password" show-password/>
+    </el-form-item>
+    <el-form-item label="验证码">
+      <div style="display:flex">
+        <el-input  v-model="form.code" />
+        <img @click="resetCode" :src="codeUrl" alt="">
+      </div>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">login</el-button>
+      <el-button>Cancel</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+const form = reactive({name: 'admin', password: '000000', code: ''})
+
+const codeUrl = ref('api/user/code')
+
+function resetCode() {
+  codeUrl.value = codeUrl.value + '?' + Math.random()
+}
+
+function onSubmit() {
+  fetch('api/user/login', {
+    method: 'post',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(form)
+  })
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
